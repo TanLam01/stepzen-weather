dabc171f022cb8b66ee38ccfaead395ccb6ddda5
Tất Lâm <tanlam15112001@gmail.com>
2023-07-17 12:34:10 +0700

    Push code

---
+++ app/api/getWeatherSummary/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import openai from "@/openai";
+
+export async function POST(request: Request) {
+    //weatherData in the body of the post req
+    const { weatherData } = await request.json();
+
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        temperature: 0.8,
+        n: 1,
+        stream: false,
+        messages: [
+            {
+                role: 'system',
+                content: `Pretend you're a weather news presenter presenting LIVE on television. 
+                Be energetic and full of charisma. Introduce yourself as weatherAI and say you are LIVE
+                from the WEATHER Headquarters. State the city you are providing a summary for. Then give a
+                summary of todays weather only. Make it easy for viewer to understand and know what to do to prepare
+                for those weahter conditions such as wear SPF if the UI is high etc. Use the uv_index data provided to provide
+                UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.`,
+            }, {
+                role: 'user',
+                content: `Hi there, can i get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(weatherData)
+                    }`,
+            }
+        ]
+    });
+
+    const { data } = response;
+    console.log('DATA IS: ', data);
+    return NextResponse.json(data.choices[0].message)
+}
